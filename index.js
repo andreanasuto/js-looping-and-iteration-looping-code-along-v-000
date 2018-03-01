efaa1105b491b575bf64fbb41a7ea3a39d11db5c
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-01 02:44:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 function printBadges(names) {
-  for (i, i < names.lenght ) {  
+  for (i, i < names.lenght, i++) {  
     console.log("Welcome Joe! You are employee #${badge}");
     return
   }
